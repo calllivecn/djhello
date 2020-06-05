053be3d45928e234b34a410c4988c1faba5e5945
calllivecn <c-all@qq.com>
2020-06-05 15:48:30 +0800

    init and 使用 shiv 打包 django to pyz包

---
+++ src/djhello/views.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+# coding=utf-8
+# date 2020-06-04 14:12:53
+# author calllivecn <c-all@qq.com>
+
+
+#from django.generic import TemplateView
+from django.http import HttpResponse, JsonResponse
+from django.views import View
+
+
+
+class Hello(View):
+
+    def get(self, request):
+        print(f"request: {request}")
+        name = request.GET["arg1"]
+        names = ",".join(request.GET.getlist("arg1"))
+        return HttpResponse(f"hello {names}")
+
+
+    def post(self, request, *args):
+        print("request.POST: {}".format(request.POST))
+        names = ",".join(request.POST.getlist("arg1"))
+        print(f"names: {names}")
+        return HttpResponse(f"hello {names}")
